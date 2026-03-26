edfd48bb38c53b50f758d3a85ac5f5368bde6ff2
Helder Correia <tuxboy@tlab.pt>
2026-03-26 17:05:07 +0000

    [IPM-20] Maximum Daily Temperature

--- src/IpmaObservation.php
+++ src/IpmaObservation.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tlab\IpmaApi;
 
 use Tlab\IpmaApi\Observation\Biology\MolluscHarvestingProhibition;
+use Tlab\IpmaApi\Observation\Climate\MaximumDailyTemperature;
 use Tlab\IpmaApi\Observation\Seismic\SeismicInformation;
 
 class IpmaObservation
@@ -21,5 +22,12 @@ class IpmaObservation
         $apiConnector = new ApiConnector();
 
         return new MolluscHarvestingProhibition($apiConnector);
+    }
+
+    public static function createMaximumDailyTemperatureApi(): MaximumDailyTemperature
+    {
+        $apiConnector = new ApiConnector();
+
+        return new MaximumDailyTemperature($apiConnector);
     }
 }
